4cf6ae37af87ea2b1b0a08d762950c09a43bcdb6
RV <peterherve88@gmail.com>
2021-02-22 11:47:58 +0100

    Ajout footer
    Ajout d'un footer avec marques-soldes-newsletter-cb...

--- view/footer.php
+++ view/footer.php
@@ -1,2 +1,55 @@
+<footer>
+	<div class="newsletter">
+		<h2>Inscrivez vous à notre newsletter et obtenez 15% de réduction immédiate!</h2>
+		<form class="newsletter_form" action="" method="post">
+			<input type="email" name="email" value="">
+			<button type="submit" name="newsletter">Inscrivez-vous</button>
+		</form>
+	</div>
+	<div class="menus_footer">
+		<div class="soldes_footer">
+			<h2>Promos</h2>
+			<ul>
+				<li><a href="#">Les hauts</a></li>
+				<li><a href="#">Les bas</a></li>
+				<li><a href="#">Les souvêtements</a></li>
+				<li><a href="#">Les accessoires</a></li>
+				<li><a href="#">Les chaussures</a></li>
+			</ul>
+		</div>
+		<div class="marques_footer">
+			<h2>Nos marques</h2>
+			<ul>
+				<li><a href="#">Lanvin</a></li>
+				<li><a href="#">Vuitton</a></li>
+				<li><a href="#">Dolce & Gabana</a></li>
+				<li><a href="#">Levis</a></li>
+				<li><a href="#">Chevignon</a></li>
+			</ul>
+		</div>
+		<div class="paiement_footer">
+			<h2>Nos moyens de paiement</h2>
+			<ul>
+				<li><img src="<?= ASSETS."footer/amex-89x75.gif"?>" alt="amex"></li>
+				<li><img src="<?= ASSETS."footer/mastercard-180x113.gif"?>" alt="mastercard"></li>
+				<li><img src="<?= ASSETS."footer/paypal-180x113.gif"?>" alt="paypal"></li>
+				<li><img src="<?= ASSETS."footer/visa-180x58.gif"?>" alt="visa"></li>
+			</ul>
+		</div>
+		<div class="social">
+			<h2>Suivez nous!</h2>
+			<div class="icons">
+				<a href="facebook.com"><img src="<?= ICONS."logo-facebook.png"?>" alt="facebook"></a>
+				<a href="instagram.com"><img src="<?= ICONS."logo-insta.png"?>" alt="instagram"></a>
+				<a href="linkedin.com"><img src="<?= ICONS."logo-in.png"?>" alt="linkedin"></a>
+				<a href="twitter.com"><img src="<?= ICONS."logo-twitter.png"?>" alt="twitter"></a>
+			</div>
+		</div>
+	</div>
+	<div class="copyright">
+		<p>©Le Dressing - 2021 - Tous droits réservés</p>
+	</div>
+
+</footer>
 </body>
 </html>
