fa457d11e6e281e90b57e12fe888a523476116b2
RV <peterherve88@gmail.com>
2021-02-23 16:02:17 +0100

    admin css

--- view/admin.php
+++ view/admin.php
@@ -1,59 +1,98 @@
-<?php
+<main>
+	<section class="recherche_client">
+		<a onclick="toggle('client_form')" href="#">Rechercher un client</a>
+		<div id=client_form  style="display: none">
+			<form  method="post" action="admin">
+				<fieldset>
+					<label for="id">Id client</label>
+					<input type="text" name="id">
+				</fieldset>
+				<fieldset>
+					<label for="login">Login</label>
+					<input type="text" name="login">
+				</fieldset>
+				<fieldset>
+					<label for="Prenom">Prénom client</label>
+					<input type="text" name="prenom">
+				</fieldset>
+				<fieldset>
+					<label for="Nom">Nom client</label>
+					<input type="text" name="nom">
+				</fieldset>
+				<fieldset>
+					<label for="Email">Email client</label>
+					<input type="text" name="email">
+				</fieldset>
+				<fieldset>
+					<label for="Adress">Adresse client</label>
+					<input type="text" name="adress">
+				</fieldset>
 
+				<input type="submit" name="search_user">
+			</form>
+		</div>
+	</section>
 
-?>
+	<section>
+		<a href="#" onclick="toggle('command_form')">Rechercher une commmande</a>
+		<form id="command_form" method="post" action="admin" style="display: none">
+			<fieldset>
+				<label for="Commande">N° de commande</label>
+				<input type="text" name="order_number">
+			</fieldset>
+			<input type="submit" name="search_order">
+		</form>
+	</section>
 
-<form method="post" action="admin">
-    <h2>Rechercher un client</h2>
-    <label for="id">Id client</label>
-    <input type="text" name="id">
-    <label for="login">Login</label>
-    <input type="text" name="login">
-    <label for="Prenom">Prénom client</label>
-    <input type="text" name="prenom">
-    <label for="Nom">Nom client</label>
-    <input type="text" name="nom">
-    <label for="Email">Email client</label>
-    <input type="text" name="email">
-    <label for="Adress">Adresse client</label>
-    <input type="text" name="adress">
-    <input type="submit" name="search_user">
-</form>
+	<section >
+		<a href="#" onclick="toggle('product_form')">Ajouter un produit</a>
+		<form id="product_form" method="post" action="product-admin" style="display: none">
+			<fieldset>
+				<label for="product_category">Catégorie produit</label>
+				<select name="product_category">
+					<option>polo</option>
+					<option>tshirt</option>
+					<option>chemise</option>
+					<option>pantalon</option>
+					<option>jeans</option>
+				</select>
+			</fieldset>
+			<fieldset>
+				<label for="product_name">Nom produit</label>
+				<input type="text" name="product_name">
+			</fieldset>
+			<fieldset>
+				<label for="product_description">Description produit</label>
+				<input type="text" name="product_description">
+			</fieldset>
+			<fieldset>
+				<label for="product_color">Couleur</label>
+				<input type="text" name="product_color">
+			</fieldset>
+			<fieldset>
+				<label for="product_fabric">Matière</label>
+				<input type="text" name="product_fabric">
+			</fieldset>
+			<fieldset>
+				<label for="article_price">Prix</label>
+				<input type="number" step="any" name="product_price">
+			</fieldset>
+			<fieldset>
+				<label for="article_code">Code article</label>
+				<input type="number" name="article_code">
+			</fieldset>
+			<input type="submit" name="add_product">
+		</form>
+	</section>
 
-<form method="post" action="admin">
-    <h2>Rechercher une commmande</h2>
-    <label for="Commande">N° de commande</label>
-    <input type="text" name="order_number">
-    <input type="submit" name="search_order">
-</form>
+	<section>
+		<a href="#" onclick="toggle('product_maj')">Mettre à jour l'état de stock d'un article</a>
+		<form id="product_maj" method="post" action="product-admin" style="display: none">
+			<fieldset>
+				<input type="number" name="article_code">
+			</fieldset>
 
-<form method="post" action="product-admin">
-    <h2>Ajouter un produit</h2>
-    <label for="product_category">Catégorie produit</label>
-    <select name="product_category">
-        <option>polo</option>
-        <option>tshirt</option>
-        <option>chemise</option>
-        <option>pantalon</option>
-        <option>jeans</option>
-    </select>
-    <label for="product_name">Nom produit</label>
-    <input type="text" name="product_name">
-    <label for="product_description">Description produit</label>
-    <input type="text" name="product_description">
-    <label for="product_color">Couleur</label>
-    <input type="text" name="product_color">
-    <label for="product_fabric">Matière</label>
-    <input type="text" name="product_fabric">
-    <label for="article_price">Prix</label>
-    <input type="number" step="any" name="product_price">
-    <label for="article_code">Code article</label>
-    <input type="number" name="article_code">
-    <input type="submit" name="add_product">
-</form>
-
-<form method="post" action="product-admin">
-    <h2>Mettre à jour l'état de stock d'un article</h2>
-    <input type="number" name="article_code">
-    <input type="submit" name="stock_update">
-</form>
+			<input type="submit" name="stock_update">
+		</form>
+	</section>
+</main>
