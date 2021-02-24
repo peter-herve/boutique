ca732381552b1416dbe30373bd0030f0a2a3c90d
RV <peterherve88@gmail.com>
2021-02-24 18:52:43 +0100

    Ajout descriptions produits

--- view/shop.php
+++ view/shop.php
@@ -1,20 +1,35 @@
 <main class="shop">
 	<section class="products">
 		<article class="product">
-			<img src="https://via.placeholder.com/200x300" alt="image Produit">
+			<img src="img/store/haut/chemise/chemise1/chemise1-1.jpg" alt="image Produit" class="image_produit">
 			<div class="etiquette_solde">
 				<div class="titre">
 					<h2>Soldes!</h2>
 				</div>
 				<div class="etiquette_logo"><h3>CodeB</h3></div>
 			</div>
-			<div class="product_data">
-				<h3>Nom du produit</h3><span>29,90€</span>
+			<div class="etiquette_vetement">
+				<h3 class="prix"><strike>39.90€</strike></h3>
+				<h3 class="reduc"> Soldes : <strong>29.90€</strong></h3>
+				<hr>
+				<h2 class="nom_produit">Nom du produit</h2>
+				<h2 class="marque">Marque</h2>
+				<hr>
+				<div class="avis">
+					<span class="material-icons small yellow">star star star star_half star_border </span>
+					<!-- <span class="material-icons small">star</span> -->
+					<h4 class="avis">293 avis</h4>
+				</div>
+				<span class="material-icons md-18">dry_cleaning local_offer local_laundry_service</span>
+
 				<p>Ceci est la description de ce produit incroyable</p>
+
+				<a class="ajout_panier" href="order?productid=1">Voir le produit</a>
+				<!-- <a class=" ajout_panier" href="order?productid=1" title="Ajouter au panier"><span class="material-icons">add_shopping_cart</span></a> -->
 			</div>
 		</article>
 		<article class="product">
-			<img src="https://via.placeholder.com/200x300" alt="image Produit">
+			<img src="img/store/haut/chemise/chemise2/chemise2-1.jpg" alt="image Produit" class="image_produit">
 			<div class="product_data">
 				<h3>Nom du produit</h3><span>29,90€</span>
 				<p>Ceci est la description de ce produit incroyable</p>
