95413996d16c64661c828567245f46b06629e322
RV <peterherve88@gmail.com>
2021-03-31 09:57:15 +0200

    Modif css Connexion

--- view/user/connexion.php
+++ view/user/connexion.php
@@ -1,20 +1,29 @@
+<div class="container-md">
+	<div class="row d-flex">
 
-<form method="post" action="connexion">
-	<?php if (isset($this->errors['login_unknown'])): ?>
-		<h3 class="alert">Login inconnu...</h3>
-	<?php endif; ?>
-	<?php if (isset($this->errors['login_empty'])): ?>
-		<h3 class="alert">Le login ne peut être vide</h3>
-	<?php endif; ?>
-	<label for="login">Login</label>
-	<input type="text" name="login">
-	<?php if (isset($this->errors['incorrect_password'])): ?>
-		<h4 class="alert">Mot de passe incorrect...</h4>
-	<?php endif; ?>
-	<?php if (isset($this->errors['password_empty'])): ?>
-		<h3 class="alert">Le mot de passe ne peut être vide</h3>
-	<?php endif; ?>
-	<label for="password">Password</label>
-	<input type="password" name="password">
-	<input type="submit" name="submit">
-</form>
+	<h1 class="text-center">Connectez-vous</h1>
+
+	<form class="col-md-6 offset-md-3 col-sm-12" method="post" action="connexion">
+		<?php if (isset($this->errors['login_unknown'])): ?>
+			<div class="alert alert-primary" role="alert">Login inconnu...</div>
+		<?php endif; ?>
+		<?php if (isset($this->errors['login_empty'])): ?>
+			<div class="alert alert-primary" role="alert">Le login ne peut être vide</div>
+		<?php endif; ?>
+		<label class="col-12 form-label" for="login">Login</label>
+		<input class="col-12 form-control" type="text" name="login">
+		<?php if (isset($this->errors['incorrect_password'])): ?>
+			<div class="alert alert-primary" role="alert">Mot de passe incorrect...</div>
+		<?php endif; ?>
+		<?php if (isset($this->errors['password_empty'])): ?>
+			<div class="alert alert-primary" role="alert">Le mot de passe ne peut être vide</div>
+		<?php endif; ?>
+		<label class="col-12 form-label" for="password">Password</label>
+		<input class="col-12 form-control"  type="password" name="password">
+		<input class="col-12 btn btn-secondary"  type="submit" name="submit">
+	</form>
+
+
+	</div>
+
+</div>
--- view/user/inscription.php
+++ view/user/inscription.php
@@ -1,4 +1,5 @@
-<main>
+<div class="container">
+
 
 <?php if (isset($this->success['register'])): ?>
 	<div class="success">
@@ -8,67 +9,72 @@
 	</div>
 
 <?php else: ?>
-<form method="post" action="inscription">
-	<?php if (isset($this->errors['field_empty'])): ?>
-		<h3 class="alert">Un champs est vide</h3>
-	<?php endif; ?>
-	<?php if (isset($this->errors['user_exists'])): ?>
-		<h3 class="alert">Login indisponible</h3>
-	<?php endif; ?>
-    <label for="login">Login</label>
-    <input type="text" name="login" id="login">
 
-    <select id="sexe" name="sexe">
-        <option></option>
-        <option>Mr</option>
-        <option>Mme</option>
-    </select>
+<div class="card col-md-9 col-sm-12 mb-3">
 
-    <label for="prenom">Prénom</label>
-    <input type="text" name="prenom" id="prenom">
+	<h1 class="card-title text-center">Inscription</h1>
 
-    <label for="nom">Nom</label>
-    <input type="text" name="nom" id="nom">
+	<form method="post" action="inscription">
+		<?php if (isset($this->errors['field_empty'])): ?>
+			<div class="alert alert-primary" role="alert">Un champs est vide</div>
+		<?php endif; ?>
+		<?php if (isset($this->errors['user_exists'])): ?>
+			<div class="alert alert-primary" role="alert">Login indisponible</div>
+		<?php endif; ?>
+	    <label for="login">Login</label>
+	    <input type="text" name="login" id="login">
 
-    <label for="adress">N° et nom de voie</label>
-    <input type="text" name="adress" id="adress">
+	    <select id="sexe" name="sexe">
+	        <option></option>
+	        <option>Mr</option>
+	        <option>Mme</option>
+	    </select>
 
-    <label for="city">Ville</label>
-    <input type="text" name="city" id="city">
+	    <label for="prenom">Prénom</label>
+	    <input type="text" name="prenom" id="prenom">
 
-    <label for="zip_code">Code Postal</label>
-    <input type="number" name="zip_code" id="zip_code" min="01000" max="99999">
+	    <label for="nom">Nom</label>
+	    <input type="text" name="nom" id="nom">
 
-    <label for="email">Email</label>
-    <input type="email" name="email" id="email">
+	    <label for="adress">N° et nom de voie</label>
+	    <input type="text" name="adress" id="adress">
 
-    <label for="password">Mot de passe</label>
-    <input type="password" name="password" id="password">
+	    <label for="city">Ville</label>
+	    <input type="text" name="city" id="city">
 
-    <label for="size">Votre taille</label>
-    <select name="top_size" id="top_size">
-        <option></option>
-        <option>XS</option>
-        <option>S</option>
-        <option>M</option>
-        <option>L</option>
-        <option>XL</option>
-        <option>XXL</option>
-    </select>
+	    <label for="zip_code">Code Postal</label>
+	    <input type="number" name="zip_code" id="zip_code" min="01000" max="99999">
 
-    <select name="bottom_size" id="bottom_size">
-        <option></option>
-        <option>36</option>
-        <option>38</option>
-        <option>40</option>
-        <option>42</option>
-        <option>44</option>
-        <option>46</option>
-    </select>
+	    <label for="email">Email</label>
+	    <input type="email" name="email" id="email">
 
-    <input type="submit" value="submit" name="submit">
-</form>
+	    <label for="password">Mot de passe</label>
+	    <input type="password" name="password" id="password">
+
+	    <label for="size">Votre taille</label>
+	    <select name="top_size" id="top_size">
+	        <option></option>
+	        <option>XS</option>
+	        <option>S</option>
+	        <option>M</option>
+	        <option>L</option>
+	        <option>XL</option>
+	        <option>XXL</option>
+	    </select>
+
+	    <select name="bottom_size" id="bottom_size">
+	        <option></option>
+	        <option>36</option>
+	        <option>38</option>
+	        <option>40</option>
+	        <option>42</option>
+	        <option>44</option>
+	        <option>46</option>
+	    </select>
+
+	    <input type="submit" value="submit" name="submit">
+	</form>
 
 <?php endif; ?>
 
-</main>
+</div>
