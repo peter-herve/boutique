370ca6d6d815a7b1c9b83c49834548699928d261
RV <peterherve88@gmail.com>
2021-04-01 13:56:23 +0200

    Validation Projet

--- Boutique.sql
+++ Boutique.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : ven. 26 mars 2021 à 08:04
+-- Généré le : mer. 31 mars 2021 à 18:33
 -- Version du serveur :  5.7.30
 -- Version de PHP : 7.4.9
 
@@ -35,7 +35,7 @@ CREATE TABLE `articles` (
   `article_fabric` varchar(26) DEFAULT NULL,
   `article_price` varchar(5) DEFAULT NULL,
   `date_added` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `article_code` int(6) DEFAULT NULL
+  `article_code` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
@@ -43,33 +43,33 @@ CREATE TABLE `articles` (
 --
 
 INSERT INTO `articles` (`id`, `category_name`, `article_name`, `article_description`, `article_color`, `article_fabric`, `article_price`, `date_added`, `article_code`) VALUES
-(1, 'polo', 'Polo 1', 'Polo MC. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'blanc', 'coton', '19,99', '2021-03-20 23:00:00.000000', 100000),
-(2, 'polo', 'Polo 2', 'Polo MC motif exotique', 'bleu', 'coton/polyester', '24,99', '2021-02-24 23:00:00.000000', 100001),
-(3, 'polo', 'Polo 3', 'Polo coupe droite', 'gris', 'coton', '14,99', '2021-02-24 23:00:00.000000', 100002),
+(1, 'polo', 'Polo 1', 'Polo MC. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'blanc', 'coton', '19,99', '2021-03-29 22:00:00.000000', 100000),
+(2, 'polo', 'Polo 2', 'Polo MC motif exotique', 'bleu', 'coton/polyester', '24,99', '2021-03-29 22:00:00.000000', 100001),
+(3, 'polo', 'Polo 3', 'Polo coupe droite', 'gris', 'coton', '14,99', '2021-03-29 22:00:00.000000', 100002),
 (4, 'polo', 'Polo 4', 'Polo casual ML', 'rouge', 'coton', '24,99', '2021-02-24 23:00:00.000000', 100003),
 (5, 'polo', 'Polo 5', 'Polo col officier', 'bleu', 'coton', '19,99', '2021-02-24 23:00:00.000000', 100004),
-(6, 'polo', 'Polo 6', 'Polo ML', 'noir', 'coton', '16,99', '2021-02-24 23:00:00.000000', 100005),
+(6, 'polo', 'Polo 6', 'Polo ML', 'noir', 'coton', '16,99', '2021-03-28 22:00:00.000000', 100005),
 (7, 'tshirt', 'T-shirt 1', 'T-shirt premium en coton bio', 'blanc', 'coton', '14,99', '2021-02-24 23:00:00.000000', 100008),
 (8, 'tshirt', 'T-shirt 2', 'T-shirt col rond', 'vert', 'coton', '11,99', '2021-02-24 23:00:00.000000', 100009),
-(9, 'tshirt', 'T-shirt 3', 'T-shirt relief ML', 'beige', 'coton/polyester', '25,99', '2021-02-24 23:00:00.000000', 100010),
-(10, 'tshirt', 'T-shirt 4', 'T-shirt matière fantaisie', 'bleu', 'coton/polyester', '9,99', '2021-02-24 23:00:00.000000', 100011),
+(9, 'tshirt', 'T-shirt 3', 'T-shirt relief ML', 'beige', 'coton/polyester', '25,99', '2021-03-29 22:00:00.000000', 100010),
+(10, 'tshirt', 'T-shirt 4', 'T-shirt matière fantaisie', 'bleu', 'coton/polyester', '9,99', '2021-03-29 22:00:00.000000', 100011),
 (11, 'tshirt', 'T-shirt 5', 'T-shirt imprimé', 'jaune', 'coton', '13,99', '2021-02-24 23:00:00.000000', 100012),
-(12, 'tshirt', 'T-shirt 6', 'T-shirt à poches', 'blanc', 'coton/polyester', '11,99', '2021-03-22 23:00:00.000000', 100013),
+(12, 'tshirt', 'T-shirt 6', 'T-shirt à poches', 'blanc', 'coton/polyester', '11,99', '2021-03-29 22:00:00.000000', 100013),
 (13, 'chemise', 'Chemise 1', 'Chemise slim unie', 'blanc', 'coton/elasthane', '19,99', '2021-02-24 23:00:00.000000', 100020),
-(14, 'chemise', 'Chemise 2', 'Chemise slim unie', 'bleu', 'coton/elasthane', '19,99', '2021-02-24 23:00:00.000000', 100021),
-(15, 'chemise', 'Chemise 3', 'Chemise slim unie', 'rouge', 'coton/elasthane', '19,99', '2021-02-24 23:00:00.000000', 100022),
+(14, 'chemise', 'Chemise 2', 'Chemise slim unie', 'bleu', 'coton/elasthane', '19,99', '2021-03-29 22:00:00.000000', 100021),
+(15, 'chemise', 'Chemise 3', 'Chemise slim unie', 'rouge', 'coton/elasthane', '19,99', '2021-03-29 22:00:00.000000', 100022),
 (16, 'pantalon', 'Pantalon 1', 'Pantalon chino regular', 'bleu', 'coton/elasthane', '24,99', '2021-02-24 23:00:00.000000', 100040),
-(17, 'pantalon', 'Pantalon 2', 'Pantalon chino slim', 'prune', 'coton/elasthane', '22,99', '2021-02-24 23:00:00.000000', 100041),
-(18, 'pantalon', 'Pantalon 3', 'Pantalon cargo clim', 'kaki', 'coton/elasthane', '24,99', '2021-02-24 23:00:00.000000', 100042),
-(19, 'jeans', 'Jeans 1', 'Jean slim délavé', 'bleu', 'coton/polyesther/elasthane', '39,99', '2021-03-21 23:00:00.000000', 100043),
+(17, 'pantalon', 'Pantalon 2', 'Pantalon chino slim', 'prune', 'coton/elasthane', '22,99', '2021-03-29 22:00:00.000000', 100041),
+(18, 'pantalon', 'Pantalon 3', 'Pantalon cargo clim', 'kaki', 'coton/elasthane', '24,99', '2021-03-29 22:00:00.000000', 100042),
+(19, 'jeans', 'Jeans 1', 'Jean slim délavé', 'bleu', 'coton/polyesther/elasthane', '39,99', '2021-03-29 22:00:00.000000', 100043),
 (20, 'jeans', 'Jeans 2', 'Jean slim brut', 'bleu', 'coton/polyesther/elasthane', '39,99', '2021-02-24 23:00:00.000000', 100044),
 (21, 'jeans', 'Jeans 3', 'Jean gris délavé', 'gris', 'coton/polyesther/elasthane', '39,99', '2021-02-24 23:00:00.000000', 100045),
-(22, 'short', 'Short 1', 'Short chino', 'bleu clair', 'coton', '24,99', '2021-02-24 23:00:00.000000', 100046),
-(23, 'short', 'Short 2', 'Short en jean', 'bleu', 'coton/elasthane', '34,99', '2021-02-24 23:00:00.000000', 100047),
-(24, 'short', 'Short 3', 'Short cargo avec ceinture', 'gris', 'coton', '34,99', '2021-02-24 23:00:00.000000', 100048),
+(22, 'short', 'Short 1', 'Short chino', 'bleu clair', 'coton', '24,99', '2021-03-29 22:00:00.000000', 100046),
+(23, 'short', 'Short 2', 'Short en jean', 'bleu', 'coton/elasthane', '34,99', '2021-03-29 22:00:00.000000', 100047),
+(24, 'short', 'Short 3', 'Short cargo avec ceinture', 'gris', 'coton', '34,99', '2021-03-29 22:00:00.000000', 100048),
 (41, 'polo', 'zed', 'test', 'test', 'test', '29.99', '2021-03-02 09:41:40.463474', 522000),
-(40, 'polo', 'azeaz', 'azeaze', 'aze', 'azea', '89', '2021-03-01 07:43:08.570110', 878987),
-(39, 'jeans', 'test', 'test', 'test', 'test', '96', '2021-02-25 15:47:34.523190', 111111),
+(40, 'polo', 'azeaz', 'azeaze', 'aze', 'azea', '89', '2021-03-29 22:00:00.000000', 878987),
+(39, 'jeans', 'test', 'test', 'test', 'test', '96', '2021-03-29 22:00:00.000000', 111111),
 (37, 'jeans', 'jean', 'jean', 'jean', 'jean', '89', '2021-02-25 15:43:29.291885', 874633),
 (38, 'polo', 'jean', 'jean', 'jean', 'zed', '87', '2021-02-25 15:45:39.492961', 555555);
 
@@ -250,6 +250,17 @@ INSERT INTO `article_stock` (`id`, `article_id`, `article_name`, `article_price`
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `brand`
+--
+
+CREATE TABLE `brand` (
+  `id` int(11) NOT NULL,
+  `brand_name` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `category`
 --
 
@@ -324,10 +335,6 @@ INSERT INTO `comments` (`id`, `article_id`, `user_id`, `comment`, `comment_mothe
 (3, 1, 6, 'bravo', NULL),
 (4, 1, 6, 'Hello', NULL),
 (5, 1, 6, 'Hello', NULL),
-(6, 1, 10, 'Encore un commentaire', NULL),
-(9, 1, 10, 'kefvjzernfzelrfglzert', NULL),
-(8, 1, 10, 'test', NULL),
-(10, 1, 10, 'test test', NULL),
 (11, 12, 10, 'First !', NULL),
 (12, NULL, 10, 'jerhgjerjk', NULL),
 (13, NULL, 10, 'jerhgjerjk', NULL),
@@ -380,7 +387,9 @@ INSERT INTO `like_article` (`id`, `user_id`, `rating`, `comment`, `article_id`) 
 (4, 10, NULL, NULL, 100013),
 (5, 10, NULL, NULL, 12),
 (6, 10, NULL, NULL, 19),
-(7, 10, NULL, NULL, 10);
+(7, 10, NULL, NULL, 10),
+(83, 10, NULL, NULL, 1),
+(72, 10, NULL, NULL, 5);
 
 -- --------------------------------------------------------
 
@@ -496,6 +505,57 @@ INSERT INTO `size` (`id`, `category_name`, `size_name`) VALUES
 (32, 'Short cargo avec ceinture', '44'),
 (33, 'Short cargo avec ceinture', '46');
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` varchar(25) NOT NULL,
+  `prenom` varchar(255) DEFAULT NULL,
+  `nom` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `adress` varchar(255) DEFAULT NULL,
+  `zip_code` int(11) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `sexe` varchar(255) DEFAULT NULL,
+  `top_size` text NOT NULL,
+  `bottom_size` int(2) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `last_connexion` datetime DEFAULT CURRENT_TIMESTAMP,
+  `user_confirm` varchar(255) DEFAULT NULL,
+  `admin` tinyint(1) DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `prenom`, `nom`, `email`, `password`, `adress`, `zip_code`, `city`, `sexe`, `top_size`, `bottom_size`, `created_at`, `last_connexion`, `user_confirm`, `admin`) VALUES
+(1, 'aaaa', 'aaaa', 'aaaa', 'aaa@aaaa.com', 'aaab', 'aaaa', 11111, 'aaaa', 'm', '', 0, '2021-02-11 15:01:56', '2021-02-11 15:01:56', NULL, 0),
+(3, 'bbbb', 'bbbb', 'bbbb', 'bbbb@bbbb.com', 'bbbb', 'bbbb', 13000, 'bbbb', 'Mr', 'XS', 36, '2021-02-15 13:59:15', '2021-02-15 13:59:15', NULL, 0),
+(6, 'cccc', 'cccc', 'cccc', 'cccc@cccc.com', '$2y$10$JNZrdTEftozC/FS170mQzuhcYlUBG7U9kvPvpx.0qaLj3G9GKb8wK', 'cccc', 13000, 'cccc', 'Mr', 'XS', 36, '2021-02-15 14:10:37', '2021-03-03 10:05:35', NULL, 0),
+(7, 'dddd', 'dddd', 'dddd', 'dddd@dddd.com', 'ddde', 'dddd', 1000, 'dddd', 'Mr', 'XS', 36, '2021-02-16 10:09:05', '2021-02-16 10:09:05', NULL, 0),
+(8, 'eeee', 'eeee', 'eeee', 'eeee@eeee.com', '$2y$10$WxygsWvgV.7jzQF/xeaQj.F10VsSDdtiuiJNvf.UEPZzFfRoNwwy2', 'eeee', 1000, 'eeee', 'Mr', 'XS', 36, '2021-02-16 11:26:40', '2021-02-16 11:26:40', NULL, 0),
+(9, 'zzzz', 'zzzz', 'zzzz', 'zzzz@zzzz.com', '$2y$10$txZttcNre.VujzHt9ypzau73MHHe8b0zL7VwUfyH35MQKGh.32EmK', 'zzzz', 1000, 'zzzz', 'Mr', 'XS', 36, '2021-02-19 09:03:15', '2021-02-24 18:23:16', NULL, 1),
+(10, 'peter', 'pit', 'herve', 'peterherve88@gmzil.com', '$2y$10$SS7UtU6EOcrKJFLCdOmhaOaZMv33OMij8AwsAsjJWL3Xe/YZODuq2', '48', 12334, 'Marseille', 'Mr', 'S', 38, '2021-03-03 09:11:12', '2021-03-31 15:47:42', NULL, 1),
+(17, 'yoyo', 'yoyo', 'kjbnkjnjk', 'oefhbrf@gmail.com', '$2y$10$fGQMzDCE1ZumpBXY6xQet.4PkcoqNzZBz9TI40zgQOStH1CLOa796', 'kjhvbjn', 76567, 'ljkhbjknk,', 'Mr', 'M', 40, '2021-03-31 15:25:34', '2021-03-31 15:29:55', NULL, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users_follow`
+--
+
+CREATE TABLE `users_follow` (
+  `id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `user_id_follow` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
 --
 -- Index pour les tables déchargées
 --
@@ -517,6 +577,12 @@ ALTER TABLE `article_sale`
 -- Index pour la table `article_stock`
 --
 ALTER TABLE `article_stock`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `brand`
+--
+ALTER TABLE `brand`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -571,6 +637,12 @@ ALTER TABLE `size`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
@@ -593,6 +665,12 @@ ALTER TABLE `article_stock`
   MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=158;
 
 --
+-- AUTO_INCREMENT pour la table `brand`
+--
+ALTER TABLE `brand`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT pour la table `category`
 --
 ALTER TABLE `category`
@@ -608,7 +686,7 @@ ALTER TABLE `color`
 -- AUTO_INCREMENT pour la table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT pour la table `fabric`
@@ -620,7 +698,7 @@ ALTER TABLE `fabric`
 -- AUTO_INCREMENT pour la table `like_article`
 --
 ALTER TABLE `like_article`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
 
 --
 -- AUTO_INCREMENT pour la table `orders`
@@ -639,6 +717,12 @@ ALTER TABLE `orders_details`
 --
 ALTER TABLE `size`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
