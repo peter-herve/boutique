ff5a33d754ba1bd05f33427942e5ae51660fab8f
RV <peterherve88@gmail.com>
2021-04-01 11:40:43 +0200

    Boutique Responsive

--- view/shop/shop.php
+++ view/shop/shop.php
@@ -1,6 +1,6 @@
-<div class="col-md-10 order-sm-first">
+<div class="col-md-10 order-first">
 
-<!-- 
+<!--
 		<h2 class="font-first">Meilleures ventes :</h2>
 
 		<div class="container" >
@@ -19,7 +19,7 @@
 
 		<h2 class=" font-first">Nos Soldes :</h2>
 		<div class="container" >
-		<div class="row row-cols-md-3 row-cols-sm-1" >
+		<div class="row row-cols-md-3 row-cols-1" >
 
 			<?php foreach ($this->soldes as $product): ?>
 			<div class="col">
@@ -34,7 +34,7 @@
 
 		<h2 class=" font-first">Nouveautés</h2>
 		<div class="container" >
-		<div class="row row-cols-md-3 row-cols-sm-1" >
+		<div class="row row-cols-md-3 row-cols-1" >
 
 			<?php foreach ($this->new as $product): ?>
 			<div class="col">
